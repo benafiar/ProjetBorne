83c35d58ba8ab83273308c4e66e05c968eace3cc
benafiar <56881097+benafiar@users.noreply.github.com>
2020-07-08 08:35:41 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,20 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="pageLogin")
      */
-    public function home(){
-        return $this->render(
-            'home.html.twig' 
-        );
-    }
+    public function home( AuthenticationUtils $utils){
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }  
 }
